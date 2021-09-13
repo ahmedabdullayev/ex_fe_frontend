9cfe21ff59efff933181ad632ad9f27394f2ca1e
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-09-13 17:02:31 +0300

    test requests no2

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <div id="nav">
     <router-link to="/">Categories</router-link> |
     <router-link to="/category/add">Add categories</router-link> |
