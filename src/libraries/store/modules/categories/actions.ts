a870f426cfad4a207db6da7ae02a22f81ba93e52
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-09-13 16:55:34 +0300

    test requests

--- src/libraries/store/modules/categories/actions.ts
+++ src/libraries/store/modules/categories/actions.ts
@@ -5,7 +5,7 @@ export default {
         return new Promise((resolve, reject)=>{
             axios({
                 method: 'get',
-                url: "http://127.0.0.1/categories",
+                url: "https://backendahmedabdullajev.42web.io/blogger/public/categories",
             })
                 .then((response: any)=>{
                     console.warn(response.data)
